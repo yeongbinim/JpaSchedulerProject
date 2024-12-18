309be5ec6dd8a6e7e8b69ef1ab72f809e58e1009
yeongbinim <yeim.dev@gmail.com>
2024-12-18 09:42:08 +0900

    init: 프로젝트 초기화

---
+++ src/main/java/yeim/jpa_scheduler/JpaSchedulerApplication.java
@@ -0,0 +1,13 @@
+package yeim.jpa_scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yeim/jpa_scheduler/JpaSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package yeim.jpa_scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
