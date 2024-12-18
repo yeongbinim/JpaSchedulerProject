d3871d356120918148a2e15f22f6c6b0f3828e64
yeongbinim <yeim.dev@gmail.com>
2024-12-18 10:36:03 +0900

    feat(lv2): Jpa기반 MemberRepository 구현

---
+++ src/main/java/yeim/jpa_scheduler/member/infrastructure/MemberRepositoryImpl.java
@@ -0,0 +1,42 @@
+package yeim.jpa_scheduler.member.infrastructure;
+
+import java.util.List;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Repository;
+import yeim.jpa_scheduler.member.domain.Member;
+
+@Repository
+@Primary
+@RequiredArgsConstructor
+public class MemberRepositoryImpl implements MemberRepository {
+
+	private final JpaMemberRepository jpaMemberRepository;
+
+	@Override
+	public Member create(Member member) {
+
+		return jpaMemberRepository.save(MemberEntity.from(member)).toModel();
+	}
+
+	@Override
+	public List<Member> findAll() {
+		return jpaMemberRepository.findAll().stream().map(MemberEntity::toModel).toList();
+	}
+
+	@Override
+	public Optional<Member> findById(Long id) {
+		return jpaMemberRepository.findById(id).map(MemberEntity::toModel);
+	}
+
+	@Override
+	public Member update(Member member) {
+		return jpaMemberRepository.save(MemberEntity.from(member)).toModel();
+	}
+
+	@Override
+	public void delete(Long id) {
+		jpaMemberRepository.deleteById(id);
+	}
+}
