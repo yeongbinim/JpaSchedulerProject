1c8fd6542fb46ba48fd6496e3301280601918d44
yeongbinim <yeim.dev@gmail.com>
2024-12-18 11:35:26 +0900

    feat(lv2): ScehduleService crud 메서드 구현

--- src/main/java/yeim/jpa_scheduler/schedule/service/ScheduleService.java
+++ src/main/java/yeim/jpa_scheduler/schedule/service/ScheduleService.java
@@ -3,6 +3,7 @@ package yeim.jpa_scheduler.schedule.service;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import yeim.jpa_scheduler.member.domain.Member;
 import yeim.jpa_scheduler.member.infrastructure.MemberRepository;
 import yeim.jpa_scheduler.schedule.domain.Schedule;
 import yeim.jpa_scheduler.schedule.domain.ScheduleCreate;
@@ -17,21 +18,27 @@ public class ScheduleService {
 	private final MemberRepository memberRepository;
 
 	public Schedule createSchedule(Long memberId, ScheduleCreate scheduleCreate) {
-		return null;
+		Member member = memberRepository.findById(memberId)
+			.orElseThrow(() -> new IllegalArgumentException("찾을 수 없습니다."));
+		Schedule schedule = Schedule.from(member, scheduleCreate);
+		return scheduleRepository.create(schedule);
 	}
 
 	public List<Schedule> getAllSchedules() {
-		return List.of();
+		return scheduleRepository.findAll();
 	}
 
 	public Schedule getSchedule(Long id) {
-		return null;
+		return scheduleRepository.findById(id)
+			.orElseThrow(() -> new IllegalArgumentException("찾을 수 없습니다."));
 	}
 
 	public Schedule updateSchedule(Long id, ScheduleUpdate scheduleUpdate) {
-		return null;
+		Schedule schedule = getSchedule(id);
+		return scheduleRepository.update(schedule.update(scheduleUpdate));
 	}
 
 	public void deleteSchedule(Long id) {
+		scheduleRepository.delete(id);
 	}
 }
